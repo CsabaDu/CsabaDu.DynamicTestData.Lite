ee65f5a2d7949d367c0795daa75c0a113a6c6ebd
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-12-15 19:15:19 +0100

    Converters

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,7 +2,7 @@
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
 global using CsabaDu.DynamicTestData.Core.DataStrategyTypes;
-global using CsabaDu.DynamicTestData.Core.Extensions;
 global using CsabaDu.DynamicTestData.Core.TestDataTypes.Interfaces;
+global using CsabaDu.DynamicTestData.Core.Validators;
 global using System.Diagnostics.CodeAnalysis;
 global using static CsabaDu.DynamicTestData.Core.TestDataTypes.Factories.TestDataFactory;
