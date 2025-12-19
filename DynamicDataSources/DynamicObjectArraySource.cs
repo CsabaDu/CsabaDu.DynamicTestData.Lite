d4a1bc674e6aa7b4a32f3aee64ea4faab9bb571c
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-12-19 11:56:39 +0100

    dg

--- DynamicDataSources/DynamicObjectArraySource.cs
+++ DynamicDataSources/DynamicObjectArraySource.cs
@@ -28,3 +28,14 @@ public abstract class DynamicObjectArraySource(ArgsCode argsCode, PropsCode prop
     => testData.ToParams(ArgsCode, PropsCode);
     #endregion
 }
+
+/// <summary>
+/// Represents a dynamic object array source, specifically configured to exclude testcase name from
+/// the object arrays when using 'ArgsCode.Properties'.
+/// </summary>
+/// <remarks>This class is an abstract base type that provides functionality for dynamically generating object
+/// arrays with a focus on expected properties. It inherits from <see cref="DynamicObjectArraySource"/> and is
+/// initialized with a specific <see cref="ArgsCode"/> instance.</remarks>
+/// <param name="argsCode">The <see cref="ArgsCode"/> instance used to configure the dynamic object array source.</param>
+public abstract class DynamicExpectedObjectArraySource(ArgsCode argsCode)
+: DynamicObjectArraySource(argsCode, PropsCode.Expected);
